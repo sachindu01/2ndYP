a46233d021bba34188dd87077419b65cbb4913ca
NisithaPadeniya <e20276@eng.pdn.ac.lk>
2024-09-07 16:01:32 +0530

    add navbar

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -3,7 +3,7 @@ import { assets } from '../assets/frontend_assets/assets';
 
 const Hero = () => {
   return (
-    <div className='flex flex-col sm:flex-row border border-grey-400'>
+    <div className='flex flex-col sm:flex-row border border-grey-400 '>
         {/*Hero left side*/ }
         <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0'>
             <div className='text-[#414141]'>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,37 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { assets } from "../assets/frontend_assets/assets";
 import { NavLink, Link } from "react-router-dom";
 
 const Navbar = () => {
+    const [visible, setVisible] = useState(false);
+    const [headerFixed, setHeaderFixed] = useState(false);
+    const [lastScrollY, setLastScrollY] = useState(0);
+
+    useEffect(() => {
+        const handleScroll = () => {
+          if (window.scrollY > 200 && window.scrollY > lastScrollY) {
+            // User is scrolling down
+            setHeaderFixed(true);
+          } else if (window.scrollY < lastScrollY) {
+            // User is scrolling up
+            setHeaderFixed(false);
+          }
+          // Set the last scroll position
+          setLastScrollY(window.scrollY);
+        };
+    
+        window.addEventListener('scroll', handleScroll);
+    
+        // Clean up the event listener on component unmount
+        return () => {
+          window.removeEventListener('scroll', handleScroll);
+        };
+      }, [lastScrollY]); 
+
+
+
   return (
-    <div className="flex items-center justify-between py-5 font-medium">
+    <div className={`flex items-center justify-between py-5 font-medium  ${headerFixed ? "navbar-fixed fadeInDown" : ""}`} >
       <Link to="/">
         <div className="flex items-center">
           <img
@@ -23,18 +50,18 @@ const Navbar = () => {
       </Link>
 
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
-        <NavLink to="/" className="flex flex-col items-center gap-1">
+        <NavLink to="/" className="flex flex-col items-center gap-1" >
           <p>HOME</p>
           <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
         </NavLink>
 
-        <NavLink to="/collection" className="flex flex-col items-center gap-1">
-          <p>INVENTORY</p>
+        <NavLink to="/collection" className="flex flex-col items-center gap-1" >
+          <p>DASHBOARD</p>
           <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
         </NavLink>
 
         <NavLink to="/orders" className="flex flex-col items-center gap-1">
-          <p>DASHBOARD</p>
+          <p>INVENTORY</p>
           <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
         </NavLink>
 
@@ -43,7 +70,52 @@ const Navbar = () => {
           <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
         </NavLink>
       </ul>
+
+      <div className="flex items-center gap-6">
+        
+        <div className="group relative">
+          <Link to="/login">
+            <img
+              className="w-5 cursor-pointer"
+              src={assets.profile_icon}
+              alt="Profile"
+            />
+          </Link>
+
+          <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4 text-gray-500">
+            <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 transition-opacity duration-200 group-hover:opacity-100">
+              <p className="cursor-pointer hover:text-black">Login</p>
+              <p className="cursor-pointer hover:text-black">Logout</p>
+            </div>
+          </div>
+        </div>
+    
+
+        <Link to="/cart" className="relative">
+          <img src={assets.cart_icon} className="w-5 min-w-5" alt="Cart" />
+          <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white rounded-full">
+            
+          </p>
+        </Link>
+
+        <img onClick={() => setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt=""/>
+      </div>
+
+      {/* Sidebar menu for small screen */}
+      <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white  ${visible ? 'w-full' : 'w-0'}`}>
+        <div className="flex flex-col text-gray-600">
+            <div onClick={() => setVisible(false)} className="flex items-center gap-4 p-3 cursor-pointer">
+                <img className="h-4 rotate-180" src={assets.dropdown_icon} alt="" />
+                <p>Back</p>
+            </div>
+        <NavLink onClick={() => setVisible(false)} className="py-2 pl-6 border" to="/">HOME</NavLink>
+        <NavLink onClick={() => setVisible(false)} className="py-2 pl-6 border" to="/dashboard">DASHBOARD</NavLink>
+        <NavLink onClick={() => setVisible(false)} className="py-2 pl-6 border" to="/inventory">INVENTORY</NavLink>
+        <NavLink onClick={() => setVisible(false)} className="py-2 pl-6 border" to="/contact">CONTACT</NavLink>
+      </div>
+            </div>
     </div>
+
   );
 };
 
