a86f687099d2b3b2c71ca85dafc3ca74f792b2b0
NisithaPadeniya <e20276@eng.pdn.ac.lk>
2024-09-11 23:49:49 +0530

    add fund-form route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,55 +1,51 @@
-import React from 'react'
-import {Routes,Route} from 'react-router-dom'
-import Home from './pages/Home'
-import Contact from './pages/Contact'
-import Dashboard from './pages/Dashboard';
-import Inventory from './pages/Inventory';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Dashboard from "./pages/Dashboard";
+import Inventory from "./pages/Inventory";
+import FundForm from "./pages/FundForm";
 
+import Collection from "./pages/Collection";
+import About from "./pages/About";
 
-
-import Collection from './pages/Collection'
-import About from './pages/About'
-
-import Product from './pages/Product'
-import Cart from './pages/Cart'
-import Login from './pages/Login'
-import PlaceOrder from './pages/PlaceOrder'
-import Orders from './pages/Orders'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import SearchBar from './components/SearchBar'
-import { ToastContainer, toast } from 'react-toastify';
-  import 'react-toastify/dist/ReactToastify.css';
-
-
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import PlaceOrder from "./pages/PlaceOrder";
+import Orders from "./pages/Orders";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import SearchBar from "./components/SearchBar";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
-    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px=[9vw]'>
-      <ToastContainer/>
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px=[9vw]">
+      <ToastContainer />
       <Navbar />
-      <SearchBar/>
+      <SearchBar />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/collection' element={<Collection/>}/>
-        <Route path='/about' element={<About/>}/>
-        
-        <Route path='/product/:productId' element={<Product/>}/>
-        <Route path='/cart' element={<Cart/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/place-order' element={<PlaceOrder/>}/>
-        <Route path='/orders' element={<Orders/>}/>
-        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/about" element={<About />} />
 
-        <Route path='/dashboard' element={<Dashboard/>}/>
-        <Route path='/inventory' element={<Inventory/>}/>
-        <Route path='/contact' element={<Contact/>}/>
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/dashboard" element={<Dashboard />} />
 
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/inventory" element={<Inventory />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/fund-form" element={<FundForm />} />
       </Routes>
-      <Footer/>
-
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ frontend/src/pages/FundForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FundForm = () => {
+  return (
+    <div>
+      fund form
+    </div>
+  )
+}
+
+export default FundForm
