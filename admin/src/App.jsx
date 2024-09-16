85ca08afdcf0e0537d45756d857e5ade6a68e90c
NisithaPadeniya <e20276@eng.pdn.ac.lk>
2024-09-16 16:22:33 +0530

    add routes of the pages

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import { Routes, Route } from 'react-router-dom'
+import Add from './pages/Add'
+import List from './pages/List'
+import Request from './pages/Request'
 
 const App = () => {
   return (
@@ -10,6 +14,15 @@ const App = () => {
         <hr/>
         <div className="flex w-full">
           <Sidebar/>
+          <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+            <Routes>
+              <Route path="/add" element={<Add/>} />
+              <Route path="/list" element={<List/>} />
+              <Route path="/request" element={<Request/>} />
+              
+            </Routes>
+
+          </div>
         </div>
       </>
     </div>
