44541fd0ff2101b150c61f13b1b0d964a2b0cae1
NisithaPadeniya <e20276@eng.pdn.ac.lk>
2024-09-16 16:43:55 +0530

    add login page

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,30 +1,36 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
-import { Routes, Route } from 'react-router-dom'
-import Add from './pages/Add'
-import List from './pages/List'
-import Request from './pages/Request'
+import { Routes, Route } from "react-router-dom";
+import Add from "./pages/Add";
+import List from "./pages/List";
+import Request from "./pages/Request";
+import { useState } from "react";
+import Login from "./components/Login";
 
 const App = () => {
+  const [token, setToken] = useState("");
+
   return (
     <div className="bg-gray-50 min-h-screen">
-      <>
-        <Navbar />
-        <hr/>
-        <div className="flex w-full">
-          <Sidebar/>
-          <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
-            <Routes>
-              <Route path="/add" element={<Add/>} />
-              <Route path="/list" element={<List/>} />
-              <Route path="/request" element={<Request/>} />
-              
-            </Routes>
-
+      {token === "" ? (
+        <Login />
+      ) : (
+        <>
+          <Navbar />
+          <hr />
+          <div className="flex w-full">
+            <Sidebar />
+            <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+              <Routes>
+                <Route path="/add" element={<Add />} />
+                <Route path="/list" element={<List />} />
+                <Route path="/request" element={<Request />} />
+              </Routes>
+            </div>
           </div>
-        </div>
-      </>
+        </>
+      )}
     </div>
   );
 };
