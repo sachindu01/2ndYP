02fd16b241d1f3a48878cff50513e06b439a33d4
NisithaPadeniya <e20276@eng.pdn.ac.lk>
2024-09-16 16:12:50 +0530

    add sidebar component

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import React from "react";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
 const App = () => {
   return (
-    <>
-      <Navbar/>   
-    </>
-  )
-}
+    <div className="bg-gray-50 min-h-screen">
+      <>
+        <Navbar />
+        <hr/>
+        <div className="flex w-full">
+          <Sidebar/>
+        </div>
+      </>
+    </div>
+  );
+};
 
-export default App
+export default App;
