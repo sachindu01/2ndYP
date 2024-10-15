5ba7187697864c0966da54feaeab053772c43577
NisithaPadeniya <e20276@eng.pdn.ac.lk>
2024-10-16 00:19:25 +0530

    create server.js

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+// import connectDB from './config/mongodb.js'
+// import connectCloudinary from './config/cloudinary.js'
+// import userRouter from './routes/userRoutes.js'
+// import productRouter from './routes/productRouter.js'
+
+
+// App config
+const app = express()
+const port = process.env.PORT || 4000
+// connectDB()
+// connectCloudinary()
+
+
+// middlewears
+
+app.use(express.json())
+app.use(cors())
+
+// api endpoints
+// app.use('/api/user',userRouter)
+// app.use('/api/product',productRouter)
+
+
+app.get('/',(req,res)=>{
+    res.send("API Working")
+})
+
+app.listen(port, ()=>console.log('Server Started on port: '+port))
