9728ec2436633f1773f172144aaa8eaddaeac193
NisithaPadeniya <e20276@eng.pdn.ac.lk>
2024-10-16 02:47:58 +0530

    connect mongoDB and cloudinary

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-// import connectDB from './config/mongodb.js'
-// import connectCloudinary from './config/cloudinary.js'
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
 // import userRouter from './routes/userRoutes.js'
 // import productRouter from './routes/productRouter.js'
 
@@ -10,8 +10,8 @@ import 'dotenv/config'
 // App config
 const app = express()
 const port = process.env.PORT || 4000
-// connectDB()
-// connectCloudinary()
+connectDB()
+connectCloudinary()
 
 
 // middlewears
