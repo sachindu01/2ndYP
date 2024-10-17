a2aba29eb8adb79b936cd5bc516553f5e1e739c2
NisithaPadeniya <e20276@eng.pdn.ac.lk>
2024-10-18 01:28:48 +0530

    set cart in backend

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
 import userRouter from './routes/userRoutes.js'
 import productRouter from './routes/productRoutes.js'
+import cartRouter from './routes/cartRoutes.js'
 
 
 // App config
@@ -22,6 +23,7 @@ app.use(cors())
 // api endpoints
 app.use('/api/user',userRouter)
 app.use('/api/product',productRouter)
+app.use('/api/cart',cartRouter)
 
 
 app.get('/',(req,res)=>{
