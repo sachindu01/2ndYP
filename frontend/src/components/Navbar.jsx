5ba550dd93bc30c55fd5a6aaa24eac256f56c0b5
NisithaPadeniya <e20276@eng.pdn.ac.lk>
2024-10-17 22:51:28 +0530

    update login fn

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -86,32 +86,39 @@ const Navbar = () => {
 
       <div className="flex items-center gap-6">
         <div className="group relative">
-          <Link to="/login">
-            <img
-              className="w-5 cursor-pointer"
-              src={assets.profile_icon}
-              alt="Profile"
-            />
-          </Link>
+          <img
+            onClick={() => (token ? null : navigate("/login"))}
+            className="w-5 cursor-pointer"
+            src={assets.profile_icon}
+            alt="Profile"
+          />
 
-          <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4 text-gray-500">
-            <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 transition-opacity duration-200 group-hover:opacity-100">
-              <Link to="/login">
-                <p className="cursor-pointer hover:text-black">Login</p>
-              </Link>
-              <Link to="/login" onClick={logout}>
-                <p className="cursor-pointer hover:text-black">Logout</p>
-              </Link>
+          {token && (
+            <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4 text-gray-500">
+              <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 transition-opacity duration-200 group-hover:opacity-100">
+                <p
+                  onClick={() => (token ? null : navigate("/login"))}
+                  className="cursor-pointer hover:text-black"
+                >
+                  Login
+                </p>
+
+                <p onClick={logout} className="cursor-pointer hover:text-black">
+                  Logout
+                </p>
+              </div>
             </div>
-          </div>
+          )}
         </div>
 
-        <Link to="/cart" className="relative">
-          <img src={assets.cart_icon} className="w-5 min-w-5" alt="Cart" />
-          <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
-            {getCartCount()}
-          </p>
-        </Link>
+        {token && (
+          <Link to="/cart" className="relative">
+            <img src={assets.cart_icon} className="w-5 min-w-5" alt="Cart" />
+            <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
+              {getCartCount()}
+            </p>
+          </Link>
+        )}
 
         <img
           onClick={() => setVisible(true)}
