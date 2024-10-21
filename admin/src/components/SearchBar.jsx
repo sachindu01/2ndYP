7d3a7990034985e15ceaeca468330e137716b612
NisithaPadeniya <e20276@eng.pdn.ac.lk>
2024-10-21 12:36:18 +0530

    update searchbar

--- admin/src/components/SearchBar.jsx
+++ admin/src/components/SearchBar.jsx
@@ -9,7 +9,7 @@ const SearchBar = ({ search, setSearch }) => {
       <div className="inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2">
         <input
           value={search}
-          onChange={(e) => setSearch(e.target.value)}
+          onChange={(e) => setSearch(e.target.value.toUpperCase())}
           className="flex-1 outline-none border-none bg-transparent text-sm"
           type="text"
           placeholder="Enter Verification Key"
